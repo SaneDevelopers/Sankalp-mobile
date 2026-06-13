6bff1530af77ddace13c1525a8e491b771771eb1
Ayush Gorlawar <ayushgorlawar91@gmail.com>
2026-06-14 04:12:10 +0530

    fix

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -6,5 +6,15 @@ module.exports = function (api) {
       // Required for react-native-reanimated v4 + react-native-worklets
       "react-native-worklets/plugin",
     ],
+    overrides: [
+      {
+        test: /[\\/]node_modules[\\/]react-native-worklets[\\/]/,
+        plugins: [
+          ["@babel/plugin-transform-class-properties", { "loose": true }],
+          ["@babel/plugin-transform-private-methods", { "loose": true }],
+          ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
+        ],
+      },
+    ],
   };
 };
